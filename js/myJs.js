543754d09c76429f82e6143d259d5bd1b0650682
Nam Duong <S3878202@rmit.edu.vn>
2022-02-15 22:55:57 +0700

    f

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/duongminhnam2020/";
+            window.location = "https://www.instagram.com/odaycomemecuauyvyneee/";
           },
         });
       }
